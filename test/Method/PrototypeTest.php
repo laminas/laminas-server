6cd77e09fd1a968b4c8b3c3b94eae3ca51d36af5
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:42:28 -0600

    Rewriting as Laminas Project package

--- test/Method/PrototypeTest.php
+++ test/Method/PrototypeTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-server for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-server/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-server/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Server\Method;
+namespace LaminasTest\Server\Method;
 
-use Zend\Server\Method;
+use Laminas\Server\Method;
 
 /**
- * Test class for \Zend\Server\Method\Prototype
+ * Test class for \Laminas\Server\Method\Prototype
  *
- * @group      Zend_Server
+ * @group      Laminas_Server
  */
 class PrototypeTest extends \PHPUnit_Framework_TestCase
 {
