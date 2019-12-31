9863c2f6a8265615ace05c24dcf728d2eb6f52d3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:42:34 -0600

    Rewriting as Laminas Project package

--- test/Reflection/ReflectionReturnValueTest.php
+++ test/Reflection/ReflectionReturnValueTest.php
@@ -1,20 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-server for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-server/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-server/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Server\Reflection;
+namespace LaminasTest\Server\Reflection;
 
-use Zend\Server\Reflection;
+use Laminas\Server\Reflection;
 
 /**
- * Test case for \Zend\Server\Reflection\ReflectionReturnValue
+ * Test case for \Laminas\Server\Reflection\ReflectionReturnValue
  *
- * @group      Zend_Server
+ * @group      Laminas_Server
  */
 class ReflectionReturnValueTest extends \PHPUnit_Framework_TestCase
 {
