1f624e34517419973889704a9614f89a0dc28778
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:42:19 -0600

    Rewriting as Laminas Project package

--- src/Reflection/Exception/ExceptionInterface.php
+++ src/Reflection/Exception/ExceptionInterface.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-server for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-server/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-server/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Server\Reflection\Exception;
+namespace Laminas\Server\Reflection\Exception;
 
-use Zend\Server\Exception\ExceptionInterface as Exception;
+use Laminas\Server\Exception\ExceptionInterface as Exception;
 
 /**
- * Zend_Server_Reflection exceptions
+ * Laminas_Server_Reflection exceptions
  */
 interface ExceptionInterface extends Exception
 {}
