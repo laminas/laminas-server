d117efa83d3ad2d55610d48f5dd0abd5ef8dc745
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:42:41 -0600

    Rewriting as Laminas Project package

--- test/DefinitionTest.php
+++ test/DefinitionTest.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-server for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-server/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-server/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Server;
+namespace LaminasTest\Server;
 
-use Zend\Server;
-use Zend\Server\Method;
+use Laminas\Server;
+use Laminas\Server\Method;
 
 /**
- * Test class for Zend\Server\Definition
+ * Test class for Laminas\Server\Definition
  *
- * @group      Zend_Server
+ * @group      Laminas_Server
  */
 class DefinitionTest extends \PHPUnit_Framework_TestCase
 {
