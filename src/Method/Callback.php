d3be601bbec713ca22495e058c76659c6d1db7b3
Matthew Weier O'Phinney <matthew@weierophinney.net>
2021-01-08 08:48:35 -0600

    qa: correct CS issue
    Remove unused import statement
    
    Signed-off-by: Matthew Weier O'Phinney <matthew@weierophinney.net>

--- src/Method/Callback.php
+++ src/Method/Callback.php
@@ -15,7 +15,6 @@ use Laminas\Server;
 use function get_class;
 use function in_array;
 use function is_array;
-use function is_callable;
 use function is_object;
 use function method_exists;
 use function sprintf;
